af81faa0457b4d4da0ece176af619fe9e3fa73bb
mhavelant <mhavelant@brainsum.com>
2016-10-14 15:56:01 +0200

    Added environment setup selector script. QAS-1

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Some colors.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NO_COLOR='\033[0m'
+
+# Greeter text.
+printf "\n\n"
+printf "Hi!\n"
+printf "This is a setup tool to make installing this project easier.\n"
+printf "${RED}Note:${NO_COLOR} Some commands require ${RED}sudo${NO_COLOR}. Please make sure you have the right permissions for it!\n"
+printf "To continue, please choose an environment:\n"
+
+# Data arrays.
+# @note: These need to be updated in parallel!
+SUPPORTED_OPTIONS=( "amazee.io" )
+SCRIPTS=( "./environment/amazee/setup.sh" )
+
+# Display options.
+for i in "${!SUPPORTED_OPTIONS[@]}"
+do
+    printf "\t%s) %s\n" "$((i+1))" "${SUPPORTED_OPTIONS[$i]}"
+done
+
+printf "\t%s) %s\n" "0" "exit"
+
+# Get a valid input.
+FINISHED=false
+while ! ${FINISHED}
+do
+    printf "%s: " "Your choice"
+    read CHOICE
+
+    if [ ${CHOICE} = 0 ]
+    then
+        printf "Bye!\n"
+        exit 0
+    fi
+
+    if [ ${SUPPORTED_OPTIONS[$((CHOICE-1))]+true} ]
+    then
+        FINISHED=true
+    else
+        printf "Invalid choice, try again!\n"
+    fi
+done
+
+printf "\n"
+
+# Run the required script.
+bash "${SCRIPTS[$((CHOICE-1))]}"
