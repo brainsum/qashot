430efce637593caedf077e6d0befd63d037fc3bb
mhavelant <mhavelant@brainsum.com>
2017-01-26 10:53:46 +0100

    Update.sh updated.

--- update-docker.sh
+++ update-docker.sh
@@ -2,7 +2,7 @@
 # Update code for dev instances using a local environment.
 git pull --recurse-submodules=yes \
     && git submodule update --recursive --remote \
-    && docker-compose exec --user 82 php sh -c \
+    && docker-compose exec php sh -c \
         "composer install && cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
         && echo "Update successful!" \
         || echo "Update failed!"
