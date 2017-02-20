b78ac1d6a1eefc8a0b35551622f1b331166b8671
mhavelant <mhavelant@brainsum.com>
2017-02-20 11:40:44 +0100

    Update scripts modified, added customized phpunit.xml.dist somewhat configed for docker.

--- update-docker-prod.sh
+++ update-docker-prod.sh
@@ -3,6 +3,6 @@
 git pull --recurse-submodules=yes \
     && git submodule update --recursive --remote \
     && docker-compose exec --user 82 php sh -c \
-        "composer install --no-dev && cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
+        "composer install --no-dev -o && cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
         && echo "Update successful!" \
         || echo "Update failed!"
--- update-prod.sh
+++ update-prod.sh
@@ -2,7 +2,7 @@
 # Update code for dev instances using a local environment.
 git pull --recurse-submodules=yes \
     && git submodule update --recursive --remote \
-    && composer install --no-dev \
+    && composer install --no-dev -o \
     && cd web \
     && drush updb -y \
     && drush cim -y \
