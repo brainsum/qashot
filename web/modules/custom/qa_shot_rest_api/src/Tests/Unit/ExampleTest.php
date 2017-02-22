6886b6981442420dfc2d2bbf610596ef644bea3e
mhavelant <mhavelant@brainsum.com>
2017-02-22 11:54:12 +0100

    Added an example test (unrelated to the project itself). Updated phpunit.xml.dist accordingly.

---
+++ web/modules/custom/qa_shot_rest_api/src/Tests/Unit/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\qa_shot_rest_api\Tests\Unit;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase {
+
+  public static function addProvider() {
+    return array(
+      array(0, 0, 0),
+      array(0, 1, 1),
+    );
+  }
+
+  /**
+   * @dataProvider addProvider
+   */
+  public function testAdd($a, $b, $c) {
+    $this->assertEquals($c, $a + $b);
+  }
+
+}
