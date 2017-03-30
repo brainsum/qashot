2b958f50d8cd244c3036332ebf59afdba7bdedf1
mhavelant <mhavelant@brainsum.com>
2017-03-30 19:25:23 +0200

    Added status to the frontpage and entity view.

---
+++ web/modules/custom/qa_shot/src/Plugin/views/field/TestQueueStatus.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\qa_shot\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("test_queue_status")
+ */
+class TestQueueStatus extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing -- to override the parent query.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['hide_alter_empty'] = ['default' => FALSE];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    /** @var \Drupal\qa_shot\Entity\QAShotTestInterface $entity */
+    $entity = $values->_entity;
+    return $entity->statusInQueue();
+  }
+
+}
