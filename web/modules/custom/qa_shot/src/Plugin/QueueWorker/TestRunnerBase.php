639a0645e73f77ad7665bdd99a11f68dd0952847
mhavelant <mhavelant@brainsum.com>
2017-03-31 17:32:28 +0200

    QAS-61 | Added maillog, finished notifications (manual unit test done, no integration test done), added todos.
    	- Refactor needed.

--- web/modules/custom/qa_shot/src/Plugin/QueueWorker/TestRunnerBase.php
+++ web/modules/custom/qa_shot/src/Plugin/QueueWorker/TestRunnerBase.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\qa_shot\Plugin\QueueWorker;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
 use Drupal\Core\Queue\RequeueException;
 use Drupal\qa_shot\Exception\QAShotBaseException;
+use Drupal\qa_shot\Service\TestNotification;
 use Drupal\qa_shot\Service\TestQueueState;
 use Drupal\qa_shot\TestBackendInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -32,15 +34,36 @@ abstract class TestRunnerBase extends QueueWorkerBase implements ContainerFactor
   protected $queueState;
 
   /**
+   * Logger service for the qa_shot logging channel.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * The QAShot Test Notification service.
+   *
+   * @var \Drupal\qa_shot\Service\TestNotification
+   */
+  protected $notification;
+
+  /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ) {
     return new static(
       $configuration,
       $plugin_id,
       $plugin_definition,
       $container->get('backstopjs.backstop'),
-      $container->get('qa_shot.test_queue_state')
+      $container->get('qa_shot.test_queue_state'),
+      $container->get('logger.factory'),
+      $container->get('qa_shot.test_notification')
     );
   }
 
@@ -57,11 +80,25 @@ abstract class TestRunnerBase extends QueueWorkerBase implements ContainerFactor
    *   The test backend (e.g BackstopJS).
    * @param \Drupal\qa_shot\Service\TestQueueState $queueState
    *   The queue state service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerChannelFactory
+   *   The logger channel factory service.
+   * @param \Drupal\qa_shot\Service\TestNotification $notification
+   *   The notification service.
    */
-  public function __construct(array $configuration, $pluginId, $pluginDefinition, TestBackendInterface $testBackend, TestQueueState $queueState) {
+  public function __construct(
+    array $configuration,
+    $pluginId,
+    $pluginDefinition,
+    TestBackendInterface $testBackend,
+    TestQueueState $queueState,
+    LoggerChannelFactoryInterface $loggerChannelFactory,
+    TestNotification $notification
+  ) {
     parent::__construct($configuration, $pluginId, $pluginDefinition);
     $this->testBackend = $testBackend;
     $this->queueState = $queueState;
+    $this->logger = $loggerChannelFactory->get('qa_shot');
+    $this->notification = $notification;
   }
 
   /**
@@ -75,11 +112,11 @@ abstract class TestRunnerBase extends QueueWorkerBase implements ContainerFactor
       $this->queueState->setToRunning($entity->id());
       $this->testBackend->runTestBySettings($entity, $data->stage);
       $this->queueState->remove($entity->id());
+      $this->notification->sendNotification($entity, 'qa_shot');
     }
     catch (QAShotBaseException $e) {
       $this->queueState->setToError($entity->id());
-      // @todo: fix.
-      \Drupal::logger('qa_shot')->error($e->getMessage());
+      $this->logger->error($e->getMessage());
       throw new RequeueException($e->getMessage(), $e->getCode(), $e);
     }
   }
--- web/modules/custom/qa_shot/src/Service/TestNotification.php
+++ web/modules/custom/qa_shot/src/Service/TestNotification.php
@@ -2,15 +2,11 @@
 
 namespace Drupal\qa_shot\Service;
 
-use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Mail\MailManagerInterface;
 use Drupal\Core\Routing\UrlGeneratorInterface;
-use Drupal\Core\State\StateInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\qa_shot\Entity\QAShotTestInterface;
 
 /**
@@ -19,8 +15,6 @@ use Drupal\qa_shot\Entity\QAShotTestInterface;
  * @package Drupal\qa_shot\Service
  */
 class TestNotification {
-
-  use StringTranslationTrait;
 
   /**
    * Mail manager service parameter. If true, emails are sent right away.
@@ -32,14 +26,14 @@ class TestNotification {
   /**
    * The language manager service.
    *
-   * @var LanguageManagerInterface
+   * @var \Drupal\Core\Language\LanguageManagerInterface
    */
   private $languageManager;
 
   /**
    * The mail manager service.
    *
-   * @var MailManagerInterface
+   * @var \Drupal\Core\Language\LanguageManagerInterface
    */
   private $mailManager;
 
@@ -56,13 +50,6 @@ class TestNotification {
    * @var \Drupal\Core\Routing\UrlGeneratorInterface
    */
   private $urlGenerator;
-
-  /**
-   * The entity type manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  private $entityTypeManager;
 
   /**
    * The site email.
@@ -86,61 +73,119 @@ class TestNotification {
    * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerChannelFactory
    * @param \Drupal\Core\Routing\UrlGeneratorInterface $urlGenerator
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    */
   public function __construct(
     LanguageManagerInterface $languageManager,
     MailManagerInterface $mailManager,
     ConfigFactoryInterface $configFactory,
     LoggerChannelFactoryInterface $loggerChannelFactory,
-    UrlGeneratorInterface $urlGenerator,
-    EntityTypeManagerInterface $entityTypeManager) {
+    UrlGeneratorInterface $urlGenerator
+  ) {
     $this->languageManager = $languageManager;
     $this->mailManager = $mailManager;
-    $this->logger = $loggerChannelFactory->get('tieto_notification');
+    $this->logger = $loggerChannelFactory->get('qa_shot');
     $this->urlGenerator = $urlGenerator;
-    $this->entityTypeManager = $entityTypeManager;
 
     $this->siteMail = $configFactory->get('system.site')->get('mail');
     $this->siteName = $configFactory->get('system.site')->get('name');
   }
 
   /**
+   * Send the notification.
+   *
    * @param \Drupal\qa_shot\Entity\QAShotTestInterface $entity
+   *   The test entity.
    * @param string $module
+   *   The module where the processing hook_mail() resides.
    */
   public function sendNotification(QAShotTestInterface $entity, $module = 'qa_shot') {
-    $to = "almafa@gmail.com";
+    $metadata = $entity->getLastRunMetadataValue();
+
+    // @todo: FIXME, code below is quick and dirty.
+    if (empty($metadata)) {
+      return;
+    }
+
+    // In case of befor/after, only the after contains passed/failed data.
+    if ($entity->bundle() === 'before_after') {
+      $shouldSend = FALSE;
+      foreach ($metadata as $meta) {
+        if ($meta['stage'] === 'after') {
+          $shouldSend = TRUE;
+          break;
+        }
+      }
+
+      if (FALSE === $shouldSend) {
+        return;
+      }
+    }
+
+    $testResults = [];
+    $stageKey = $entity->bundle() === 'a_b' ? NULL : 'after';
+    foreach ($metadata as $meta) {
+      if ($meta['stage'] === $stageKey) {
+        $testResults = $meta;
+        break;
+      }
+    }
+
+    $passPercentage = (int) $testResults['passed_count'] / ((int) $testResults['passed_count'] + (int) $testResults['failed_count']) * 100;
+
+    $entityTypeId = $entity->getEntityTypeId();
+
+    try {
+      $link = $this->urlGenerator->generateFromRoute(
+        $entity->toUrl()->getRouteName(),
+        [$entityTypeId => $entity->id()],
+        ['absolute' => TRUE],
+        TRUE
+      )->getGeneratedUrl();
+    }
+    catch (\Exception $exception) {
+      $this->logger->error(
+        'Notification could not be sent. Error message: @msg',
+        ['@msg' => $exception->getMessage()]
+      );
+      return;
+    }
 
     $params = [
-      'subject' => '',
-      'body' => '',
+      'subject' => $entity->getName() . ', ' . $passPercentage . '% PASSED',
+      'body' => [$link],
     ];
 
+    // @todo: get the field that contains the user who started the test.
+    // Run Initiator field.
+    $receiver = $entity->getOwner()->getEmail();
 
     $result = $this->mailManager->mail(
       $module,
       $this::NOTIFICATION_MAIL_KEY,
-      $to,
+      $receiver,
       $this->languageManager->getCurrentLanguage()->getId(),
       $params,
       $this->siteMail,
       $this::SEND_NOW
     );
 
+    $logParameters = [
+      '@status' => 'fail',
+      '@id' => $entity->id(),
+    ];
+
     if ($result['result'] === TRUE) {
-      return TRUE;
+      $this->logger->notice(
+        'Notification about the @status of the test with id #@id has been sent.',
+        $logParameters
+      );
     }
     else {
-      return FALSE;
+      $this->logger->warning(
+        'Notification about the @status of the test with id #@id has not been sent.',
+        $logParameters
+      );
     }
-  }
-
-  private function composeMessage() {
-    $from = "site.mail";
-    $to = "whoever started the test";
-    $subject = 'Test "{test name}" finished with a X% success rate.';
-    $body = 'entity link';
   }
 
 }
