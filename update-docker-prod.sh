c0be74d73e2765cad86fb77d661c4863bac3a804
mhavelant <mhavelant@brainsum.com>
2017-04-19 18:02:16 +0200

    Update script fixes.

--- update-docker-prod.sh
+++ update-docker-prod.sh
@@ -2,7 +2,8 @@
 # Update code for dev instances using a local environment.
 git pull --recurse-submodules=yes \
     && git submodule update --recursive --remote \
+    && composer install --no-dev -o \
     && docker-compose exec --user 82 php sh -c \
-        "composer install --no-dev -o && cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
+        "cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
         && echo "Update successful!" \
         || echo "Update failed!"
--- update-docker.sh
+++ update-docker.sh
@@ -2,7 +2,8 @@
 # Update code for dev instances using a local environment.
 git pull --recurse-submodules=yes \
     && git submodule update --recursive --remote \
+    && composer install \
     && docker-compose exec php sh -c \
-        "composer install && cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
+        "cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
         && echo "Update successful!" \
         || echo "Update failed!"
