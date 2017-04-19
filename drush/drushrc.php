b3110a121c7803971b34c6fb5466deafe447556b
mhavelant <mhavelant@brainsum.com>
2017-04-19 15:21:04 +0200

    Set drush site uri in case of running from docker4drupal.

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * @file
+ * Drush rc file.
+ */
+
 if (getenv('AMAZEEIO_BASE_URL')) {
   $options['uri'] = getenv('AMAZEEIO_BASE_URL');
+}
+
+if (getenv("DOCKER_ENVIRONMENT") === 'docker4drupal') {
+  $options['uri'] = 'http://localhost:8000';
 }
