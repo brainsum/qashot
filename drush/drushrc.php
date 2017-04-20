34085a5d9d124d0a9264beb17785f5119488cd94
mhavelant <mhavelant@brainsum.com>
2017-04-20 17:42:25 +0200

    Removed drushcr stuff.

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -8,7 +8,3 @@
 if (getenv('AMAZEEIO_BASE_URL')) {
   $options['uri'] = getenv('AMAZEEIO_BASE_URL');
 }
-
-if (getenv("DOCKER_ENVIRONMENT") === 'docker4drupal') {
-  $options['uri'] = 'http://localhost:8000';
-}
