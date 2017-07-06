7c608105d3bddff0cef7578cdf1c597aa39b8a4c
mhavelant <mhavelant@brainsum.com>
2017-07-06 14:32:28 +0200

    Druapl_set_message replaced with echo for instant log output.

--- web/modules/custom/qa_shot/src/Cli/CliQueueRunner.php
+++ web/modules/custom/qa_shot/src/Cli/CliQueueRunner.php
@@ -14,9 +14,9 @@ class CliQueueRunner {
    */
   public function execute() {
     $date = (new \DateTime())->setTimestamp(time());
-    drupal_set_message(t('Runner script initiated at @datetime', [
+    echo t('Runner script initiated at @datetime', [
       '@datetime' => $date->format('Y-m-d H:i:s'),
-    ]));
+    ]) . "\n";
 
     /** @var \Drupal\qa_shot\Queue\QAShotQueueRunner $runner */
     $runner = \Drupal::service('qa_shot.test_queue_runner');
@@ -28,46 +28,46 @@ class CliQueueRunner {
       $itemCount = $queueInstance->numberOfItems();
 
       if (0 === $itemCount) {
-        drupal_set_message(t('No items in the @queue queue, skipping.', [
+        echo t('No items in the @queue queue, skipping.', [
           '@queue' => $queue['id'],
-        ]));
+        ]) . "\n";
         continue;
       }
 
-      drupal_set_message(t('Pre-run: Number of tests currently in the queue: @number', [
+      echo t('Pre-run: Number of tests currently in the queue: @number', [
         '@number' => $itemCount,
-      ]));
+      ]) . "\n";
 
       $runningItems = $queueInstance->numberOfRunningItems();
       if (0 < $runningItems) {
-        drupal_set_message(t('The queue @queue has already @count/@limit item(s) running. Skipping.', [
+        echo t('The queue @queue has already @count/@limit item(s) running. Skipping.', [
           '@queue' => $queue['id'],
           '@count' => $runningItems,
           '@limit' => 1,
-        ]));
+        ]) . "\n";
         continue;
       }
 
       $limit = isset($queue['cron']['time']) ? $queue['cron']['time'] : 15;
       $count += $runner->run($queue['id'], $limit);
 
-      drupal_set_message(t('Post-run: Number of tests currently in the queue: @number', [
+      echo t('Post-run: Number of tests currently in the queue: @number', [
         '@number' => $queueInstance->numberOfItems(),
-      ]));
+      ]) . "\n";
     }
 
     if ($count === 1) {
-      drupal_set_message(t('QAShot queues executed, @count item has been processed.', [
+      echo t('QAShot queues executed, @count item has been processed.', [
         '@count' => $count,
-      ]));
+      ]) . "\n";
     }
     else {
-      drupal_set_message(t('QAShot queues executed, @count items have been processed.', [
+      echo t('QAShot queues executed, @count items have been processed.', [
         '@count' => $count,
-      ]));
+      ]) . "\n";
     }
 
-    drupal_set_message("\n");
+    echo "\n";
   }
 
 }
