1bf44011b6865f752af959e6beffe5fdfdbbdd35
mhavelant <mhavelant@brainsum.com>
2017-07-06 14:55:50 +0200

    Fix for standard docker update files.

--- update-docker-prod.sh
+++ update-docker-prod.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 # Update code for dev instances using a local environment.
-git pull --recurse-submodules=yes \
-    && git submodule update --recursive --remote \
+git pull \
     && composer install --no-dev -o \
-    && docker-compose exec --user 82 php sh -c \
+    && docker-compose exec --user 33 php sh -c \
         "cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
         && echo "Update successful!" \
         || echo "Update failed!"
--- update-docker.sh
+++ update-docker.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 # Update code for dev instances using a local environment.
-git pull --recurse-submodules=yes \
-    && git submodule update --recursive --remote \
+git pull \
     && composer install \
-    && docker-compose exec php sh -c \
+    && docker-compose exec --user 33 php sh -c \
         "cd web && drush updb -y && drush cim -y && drush entity-updates -y" \
         && echo "Update successful!" \
         || echo "Update failed!"
