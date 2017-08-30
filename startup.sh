dc193ed9fe84e8c2017d1e4a59e496dda1bdc819
mhavelant <mhavelant@brainsum.com>
2017-08-30 11:19:46 +0200

    Fix user in startup.sh.

--- startup.sh
+++ startup.sh
@@ -17,5 +17,5 @@ fi
 # Start our drupal instance, ps to check if it started and open it.
 docker-compose up -d || exit 1
 docker-compose ps || exit 1
-docker-compose exec --user=82 php bash || exit 1
+docker-compose exec --user=33 php bash || exit 1
 #docker-compose exec --user=82 php sh || exit 1
