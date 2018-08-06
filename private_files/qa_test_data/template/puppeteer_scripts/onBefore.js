e19e6b90d60ce21d9692a53eb5906bde372e37c6
mhavelant <mhavelant@brainsum.com>
2018-08-06 18:28:14 +0200

    Add "puppeteer" template scripts.

---
+++ private_files/qa_test_data/template/puppeteer_scripts/onBefore.js
@@ -0,0 +1,3 @@
+module.exports = async (page, scenario, vp) => {
+    console.log('(puppeteer) onBefore.js has run for: '+ vp.name + '.');
+};
---
+++ private_files/qa_test_data/template/puppeteer_scripts/onReady.js
@@ -0,0 +1,3 @@
+module.exports = async (page, scenario, vp) => {
+    console.log('(puppeteer) onReady.js has run for: '+ vp.name + '.');
+};
