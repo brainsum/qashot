01cf910be230c45302da10c7e3e255feb3ef961c
mhavelant <mhavelant@brainsum.com>
2018-08-06 14:35:42 +0200

    Try figuring out why the compatibility check fails.

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -13,5 +13,14 @@ if [ -z "${CODE_REVIEW+x}" ] || [ "$CODE_REVIEW" -ne 1 ]; then
 fi
 
 cd "$TRAVIS_BUILD_DIR"
+echo "------- DEBUG --------"
+echo "Vendor dir: ${VENDOR_DIR}"
+echo "List vendors.."
+ls ${VENDOR_DIR}
+echo "List wimg.."
+ls ${VENDOR_DIR}/wimg/php-compatibility
+echo "------- END DEBUG ---------"
+
+composer global require hirak/prestissimo
 composer global require drupal/coder wimg/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
-phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer,~/.composer/vendor/wimg/php-compatibility
+phpcs --config-set installed_paths ${VENDOR_DIR}/drupal/coder/coder_sniffer,${VENDOR_DIR}/wimg/php-compatibility
