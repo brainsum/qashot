62d31d6fc43c5be0decf3ab8f00bff90dcc8b073
mhavelant <mhavelant@brainsum.com>
2018-08-06 14:38:07 +0200

    Travis debug.

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -12,6 +12,10 @@ if [ -z "${CODE_REVIEW+x}" ] || [ "$CODE_REVIEW" -ne 1 ]; then
  exit 0;
 fi
 
+composer global require hirak/prestissimo
+composer global require drupal/coder wimg/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
+phpcs --config-set installed_paths ${VENDOR_DIR}/drupal/coder/coder_sniffer,${VENDOR_DIR}/wimg/php-compatibility
+
 cd "$TRAVIS_BUILD_DIR"
 echo "------- DEBUG --------"
 echo "Vendor dir: ${VENDOR_DIR}"
@@ -20,7 +24,3 @@ ls ${VENDOR_DIR}
 echo "List wimg.."
 ls ${VENDOR_DIR}/wimg/php-compatibility
 echo "------- END DEBUG ---------"
-
-composer global require hirak/prestissimo
-composer global require drupal/coder wimg/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
-phpcs --config-set installed_paths ${VENDOR_DIR}/drupal/coder/coder_sniffer,${VENDOR_DIR}/wimg/php-compatibility
