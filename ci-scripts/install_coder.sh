3ce3052adac6bd6050c3cafdd9bd85262e8f2a12
mhavelant <mhavelant@brainsum.com>
2018-08-06 15:17:05 +0200

    Remove debug commands, use "phpcompatibility" namespace instead of "wimg".

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -13,14 +13,5 @@ if [ -z "${CODE_REVIEW+x}" ] || [ "$CODE_REVIEW" -ne 1 ]; then
 fi
 
 composer global require hirak/prestissimo
-composer global require drupal/coder wimg/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
-phpcs --config-set installed_paths ${VENDOR_DIR}/drupal/coder/coder_sniffer,${VENDOR_DIR}/wimg/php-compatibility
-
-cd "$TRAVIS_BUILD_DIR"
-echo "------- DEBUG --------"
-echo "Vendor dir: ${VENDOR_DIR}"
-echo "List vendors.."
-ls ${VENDOR_DIR}
-echo "List wimg.."
-ls ${VENDOR_DIR}/wimg/php-compatibility
-echo "------- END DEBUG ---------"
+composer global require drupal/coder phpcompatibility/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
+phpcs --config-set installed_paths ${VENDOR_DIR}/drupal/coder/coder_sniffer,${VENDOR_DIR}/phpcompatibility/php-compatibility
