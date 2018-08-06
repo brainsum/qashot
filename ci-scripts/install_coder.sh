732449fae5faf3bc3c6501063535d95ab101fe62
mhavelant <mhavelant@brainsum.com>
2018-08-06 14:09:08 +0200

    Update travis config.
    	- Use a phpcs.xml instead of running Drupal and DrupalPractice separately.

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -13,5 +13,5 @@ if [ -z "${CODE_REVIEW+x}" ] || [ "$CODE_REVIEW" -ne 1 ]; then
 fi
 
 cd "$TRAVIS_BUILD_DIR"
-composer global require drupal/coder
+composer global require drupal/coder wimg/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
--- ci-scripts/test_coder.sh
+++ ci-scripts/test_coder.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 # ---------------------------------------------------------------------------- #
 #
@@ -11,31 +12,8 @@ if [ -z "${CODE_REVIEW+x}" ] || [ "$CODE_REVIEW" -ne 1 ]; then
  exit 0;
 fi
 
-HAS_ERRORS=0
+if ! phpcs --standard=../phpcs.xml "${PHPCS_CHECK_DIRECTORY}"; then
+    exit 1;
+fi
 
-##
-# Function to run the actual code review
-#
-# This function takes 2 params:
-# @param string $1
-#   The file path to the directory or file to check.
-# @param string $2
-#   The ignore pattern(s).
-##
-code_review () {
-  echo "${LWHITE}$1${RESTORE}"
-
-  if ! phpcs --ignore="${TRAVIS_BUILD_DIR}/web/modules/custom/qa_shot/src/Service/MachineLearning.php, *.md" --standard="$REVIEW_STANDARD" -p --colors --extensions=php,module,inc,install,test,profile,md "$1"; then
-    HAS_ERRORS=1
-  fi
-}
-
-# Review custom modules, run each folder separately to avoid memory limits.
-echo
-echo "${LBLUE}> Sniffing Modules following '${REVIEW_STANDARD}' standard. ${RESTORE}"
-
-for dir in $TRAVIS_BUILD_DIR/web/modules/custom/*/ ; do
-  code_review "$dir"
-done
-
-exit $HAS_ERRORS
+exit 0;
