abc01e0ab3293e72fddb537d71310c58d760d077
mhavelant <mhavelant@brainsum.com>
2018-08-06 14:18:28 +0200

    Add lint check, fix PHPCompatibility not getting registered.

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -14,4 +14,4 @@ fi
 
 cd "$TRAVIS_BUILD_DIR"
 composer global require drupal/coder wimg/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
-phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
+phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer,~/.composer/vendor/wimg/php-compatibility
