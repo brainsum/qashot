27b5f143877dfab78f1d431a53b2978a75ce14d9
mhavelant <mhavelant@brainsum.com>
2018-09-17 21:29:22 +0200

    Add (untested) results fetcher code.

--- web/modules/custom/qa_shot/src/Cli/CliRemoteQueueRunner.php
+++ web/modules/custom/qa_shot/src/Cli/CliRemoteQueueRunner.php
@@ -18,6 +18,7 @@ class CliRemoteQueueRunner {
 
   const QUEUE_NAME = 'qa_shot_remote_queue';
   const ENDPOINT_TEST_ADD = '/api/v1/test/add';
+  const ENDPOINT_RESULT_FETCH = '/api/v1/result/fetch';
 
   /**
    * The messenger service.
@@ -97,7 +98,7 @@ class CliRemoteQueueRunner {
    */
   public function publishAll() {
     $date = (new \DateTime())->setTimestamp(time());
-    $this->messenger->addMessage($this->t('Publishing ot external queue initiated at @datetime', [
+    $this->messenger->addMessage($this->t('Publishing to external queue initiated at @datetime', [
       '@datetime' => $date->format('Y-m-d H:i:s'),
     ]));
 
@@ -116,7 +117,7 @@ class CliRemoteQueueRunner {
       return $item->tid;
     }, $items);
 
-    /** @var \Drupal\qa_shot\Entity\QAShotTestInterface $tests */
+    /** @var \Drupal\qa_shot\Entity\QAShotTestInterface[] $tests */
     $tests = $this->testStorage->loadMultiple($testIds);
 
     $successCount = 0;
@@ -197,7 +198,7 @@ class CliRemoteQueueRunner {
       'mode' => 'a_b',
       'stage' => '',
       'uuid' => $test->uuid(),
-      'origin' => 'drupal',
+      'origin' => $this->configFactory->get('qashot.settings')->get('instance_id'),
       'environment' => $this->configFactory->get('qashot.settings')->get('current_environment'),
       'test_config' => $backstopConfig,
     ];
@@ -252,28 +253,145 @@ class CliRemoteQueueRunner {
   }
 
   /**
+   * Try consuming results for an array of uuids.
+   *
+   * @param array $uuids
+   *   Uuids.
+   *
+   * @return array
+   *   Results.
+   */
+  protected function consume(array $uuids) {
+    if (NULL === $this->remoteHost) {
+      return [
+        'code' => 204,
+        'message' => $this->t('Remote host undefined, skipping.'),
+        'reason' => '',
+        'results' => [],
+      ];
+    }
+
+    $payload = [
+      'origin' => $this->configFactory->get('qashot.settings')->get('instance_id'),
+      'uuids' => $uuids,
+    ];
+
+    $requestOptions = [
+      // @todo: auth
+      'json' => $payload,
+      'connect_timeout' => 10,
+    ];
+
+    try {
+      $response = $this->httpClient->post($this->remoteHost . self::ENDPOINT_RESULT_FETCH, $requestOptions);
+    }
+    catch (ClientException $exception) {
+      $this->logger->warning('Client error! Could not fetch results from the remote worker. See: ' . $exception->getMessage());
+      $response = $exception->getResponse();
+      if (NULL === $response) {
+        return [
+          'code' => $exception->getCode(),
+          'message' => $exception->getMessage(),
+          'results' => [],
+          'reason' => '',
+          'errors' => [],
+        ];
+      }
+      $remoteMessage = \json_decode($response->getBody()->getContents(), TRUE);
+      return [
+        'code' => $response->getStatusCode(),
+        'message' => $remoteMessage['message'],
+        'reason' => $response->getReasonPhrase(),
+        'results' => [],
+        'errors' => $remoteMessage['errors'] ?? [],
+      ];
+    }
+    catch (\Exception $exception) {
+      $this->logger->warning('Unknown error! Could not fetch results from the remote worker. See: ' . $exception->getMessage());
+      return [
+        'code' => $exception->getCode(),
+        'message' => $exception->getMessage(),
+        'results' => [],
+        'reason' => '',
+        'errors' => [],
+      ];
+    }
+
+    $results = \json_decode($response->getBody()->getContents(), TRUE);
+    $this->logger->info('Results fetched from remote worker.');
+
+    return [
+      'code' => $response->getStatusCode(),
+      'results' => $results,
+      'reason' => $response->getReasonPhrase(),
+      'errors' => $remoteMessage['errors'] ?? [],
+    ];
+  }
+
+  /**
    * Consume messages from the remote queue.
    */
   public function consumeAll() {
-    $mqSettings = $this->configFactory->get('rabbitmq.settings');
+    $date = (new \DateTime())->setTimestamp(time());
+    $this->messenger->addMessage($this->t('Reading from external queue initiated at @datetime', [
+      '@datetime' => $date->format('Y-m-d H:i:s'),
+    ]));
 
-    try {
-      $mqLib = new BunnyLib(
-        $mqSettings->get('connection'),
-        $mqSettings->get('channels')
-      );
-    }
-    catch (\Exception $exception) {
-      $this->messenger->addMessage($exception->getMessage());
+    $items = $this->queue->getItems(self::QUEUE_NAME, QAShotQueue::QUEUE_STATUS_REMOTE);
+    $itemCount = \count($items);
+    $this->messenger->addMessage($this->t('The "@queue" has @count items waiting.', [
+      '@queue' => self::QUEUE_NAME,
+      '@count' => $itemCount,
+    ]));
+    if (0 === $itemCount) {
+      $this->messenger->addMessage($this->t('Skipping.'));
       return;
     }
 
-    $messages = $mqLib->consumeMultiple();
-    $messageCount = \count($messages);
-    $this->messenger->addMessage("Consumtion ended, received messages: $messageCount\n");
-    // @todo:
-    // Update test status in queue.
-    // Update test entity status.
+    $testIds = \array_map(function ($item) {
+      return $item->tid;
+    }, $items);
+
+    /** @var \Drupal\qa_shot\Entity\QAShotTestInterface[] $tests */
+    $tests = $this->testStorage->loadMultiple($testIds);
+
+    /** @var string[] $uuids */
+    $uuids = \array_map(function ($test) {
+      return $test->uuid();
+    }, $tests);
+
+
+    $results = [];
+    $uuidCount = \count($uuids);
+    for ($i = 0; $uuidCount > $i; $i += 20) {
+      $subset = \array_slice($uuids, $i, 20);
+
+      if (empty($subset)) {
+        break;
+      }
+
+      $rawResults = $this->consume($subset);
+      $results = \array_merge($rawResults['results']);
+    }
+
+    // @todo: Display/Log errors.
+    $this->logger->debug(\var_export($results, TRUE));
+
+    foreach ($uuids as $uuid) {
+      // @todo: save data for $results[$uuid];
+      // -- Fetch images from remote and save them properly.
+      // -- Update metadata properly.
+      // -- Save additional metadata as raw data or a file.
+      // @todo: if the uuid is not set and it did not error,
+      // leave it in the queue QUEUE_STATUS_REMOTE.
+      // Maybe update lease time as well.
+      // @todo: if it errored out, QUEUE_STATUS_ERROR.
+    }
+
+    $resultCount = \count($results);
+    $this->messenger->addMessage('Fetch ended.');
+    $this->messenger->addMessage("-- Requested $uuidCount results.");
+    $this->messenger->addMessage("-- Received $resultCount results.");
   }
 
 }
