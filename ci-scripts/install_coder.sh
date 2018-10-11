0fa16e9d4e52f4932e9603c1ceb7296222c72861
mhavelant <mhavelant@brainsum.com>
2018-10-11 21:23:27 +0200

    Add notification options, (hopefully) fix phpcs.

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -15,6 +15,4 @@ fi
 composer global require hirak/prestissimo:^0.3
 composer global require drupal/coder:^8.3 squizlabs/php_codesniffer:^3.3 phpcompatibility/php-compatibility:^9.0 jakub-onderka/php-parallel-lint:^1.0 jakub-onderka/php-console-highlighter:^0.4 dealerdirect/phpcodesniffer-composer-installer:^0.4
 
-ls -lsa ${VENDOR_DIR}/bin
-
-${VENDOR_DIR}/bin/phpcs --config-set installed_paths ${VENDOR_DIR}/drupal/coder/coder_sniffer,${VENDOR_DIR}/phpcompatibility/php-compatibility
+phpcs --config-set installed_paths ${VENDOR_DIR}/drupal/coder/coder_sniffer,${VENDOR_DIR}/phpcompatibility/php-compatibility
