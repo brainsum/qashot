c2bbd19c921f5bbc203ac4fba7225afba7b17a9a
mhavelant <mhavelant@brainsum.com>
2019-03-29 16:42:46 +0100

    QAP-000 | Casing and return value fixes.

--- web/modules/custom/qa_shot/src/QAShotTestHtmlRouteProvider.php
+++ web/modules/custom/qa_shot/src/QAShotTestHtmlRouteProvider.php
@@ -20,14 +20,14 @@ class QAShotTestHtmlRouteProvider extends AdminHtmlRouteProvider {
   public function getRoutes(EntityTypeInterface $entityType) {
     $collection = parent::getRoutes($entityType);
 
-    $entityType_id = $entityType->id();
+    $entityTypeId = $entityType->id();
 
-    if ($collection_route = $this->getCollectionRoute($entityType)) {
-      $collection->add("entity.{$entityType_id}.collection", $collection_route);
+    if ($collectionRoute = $this->getCollectionRoute($entityType)) {
+      $collection->add("entity.{$entityTypeId}.collection", $collectionRoute);
     }
 
-    if ($settings_form_route = $this->getSettingsFormRoute($entityType)) {
-      $collection->add("$entityType_id.settings", $settings_form_route);
+    if ($settingsFormRoute = $this->getSettingsFormRoute($entityType)) {
+      $collection->add("$entityTypeId.settings", $settingsFormRoute);
     }
 
     return $collection;
@@ -42,13 +42,13 @@ class QAShotTestHtmlRouteProvider extends AdminHtmlRouteProvider {
    * @return \Symfony\Component\Routing\Route|null
    *   The generated route, if available.
    */
-  protected function getCollectionRoute(EntityTypeInterface $entityType) {
+  protected function getCollectionRoute(EntityTypeInterface $entityType): ?Route {
     if ($entityType->hasLinkTemplate('collection') && $entityType->hasListBuilderClass()) {
-      $entityType_id = $entityType->id();
+      $entityTypeId = $entityType->id();
       $route = new Route($entityType->getLinkTemplate('collection'));
       $route
         ->setDefaults([
-          '_entity_list' => $entityType_id,
+          '_entity_list' => $entityTypeId,
           '_title' => "{$entityType->getLabel()} list",
         ])
         ->setRequirement('_permission', 'access qashot test overview')
@@ -56,6 +56,8 @@ class QAShotTestHtmlRouteProvider extends AdminHtmlRouteProvider {
 
       return $route;
     }
+
+    return NULL;
   }
 
   /**
